f44fac6e1deaa9252ba1d202e7e1a2b4c11146bb
ezthor <1035484464@qq.com>
2024-04-15 20:43:53 +0800

    封装visualizer中

--- Lidar/Visualizer.py
+++ Lidar/Visualizer.py
@@ -16,11 +16,44 @@ class Visualizer:
         self.update = update # 使用的是update_geometry还是add_geometry
         self.first = True # 是否是第一次添加几何体
 
-    def set_visualizer_pcd(self, geometry):
-        self.visualizer.add_geometry(geometry)
+    # 首次添加几何体
+    def set_visualizer_pcd(self, pcd):
+        # 如果geometry非空，则添加几何体
+        if self.is_pcd_empty(pcd):
+            self.visualizer.add_geometry(pcd)
+            self.first = False # 如果添加了pcd，则已经有pcd了不为空
+        else:
+            print("pcd is empty")
 
     # 更新几何体
-    def update_visualizer(self):
+    def update_geometry(self):
         # 自己调用update_geometry
+        if self.first:
+            print("no pcd")
+            return
         self.visualizer.update_geometry(self.pcd)
+
+    def update_pcd_points(self, pc): # 把可视化对象的pcd的points属性修改为pc
+        self.pcd.points = o3d.utility.Vector3dVector(pc)
+
+    # 每一次可视化的流程，修改vis里的geometry的points属性，然后update_geometry，然后vis.poll_events()，然后vis.update_renderer()
+
+    # 更新visualizer
+    def update_visualizer(self):
+        self.visualizer.poll_events()
+        self.visualizer.update_renderer()
+
+    # 整体update，直接调用这个
+    def update(self, pc): # 把新的点云数据传入，更新可视化
+        self.update_pcd_points(pc)
+        self.update_geometry()
+        self.update_visualizer()
+
+    # 判断geometry是否为空
+    def is_pcd_empty(self):
+        if len(self.pcd.points) == 0:
+            return True
+        else:
+            return False
+
 
