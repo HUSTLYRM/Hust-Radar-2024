6672a0c5c7bf94d3f00e048e88f5ecc66e58b8e5
ezthor <1035484464@qq.com>
2024-07-27 16:22:17 +0800

    自定义radar_log类，方便自己使用习惯加log

---
+++ Log/Log.py
@@ -0,0 +1,27 @@
+import os
+import time
+
+class RadarLog:
+    def __init__(self, test_name):
+        self.test_name = test_name
+        self.timestamp = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
+        self.log_dir = '/home/nvidia/RadarWorkspace/code/Radar_Develop/logfile'
+        self.log_path = f'{self.log_dir}/{self.test_name}_{self.timestamp}.log'
+        # 确保log目录存在
+        if not os.path.exists(self.log_dir):
+            os.makedirs(self.log_dir)
+
+    def log(self, message):
+        with open(self.log_path, 'a') as log_file:
+            # 为每条日志添加时间戳
+            timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            log_file.write(f'[{timestamp}] {message}\n')
+
+# 使用示例
+if __name__ == "__main__":
+    logger = RadarLog("test_example")
+    index = 0
+    while index < 1000:
+        if index % 100 == 0:
+            logger.log(f"Processing {index}th image...")
+        index += 1
